f486448fde809a6577893d1722bdf4a24a68f22c
Mos <apple@Zil.local>
2017-03-18 14:35:58 +0800

    新增：刪除飲料功能

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="app">
     <order-button v-for="drink in drinkList" @check="saveOrder" :name="drink"></order-button>
-    <ol>
+    <ol class="order-list">
       <span>你點的是：</span>
-      <li v-for="order in orderList">{{ print(order) }}</li>
+      <li v-for="order in orderList">
+        <span :style="{ backgroundColor: orderColor(order) }" class="order">{{ print(order) }}</span>
+        <button :style="{ backgroundColor: orderColor(order) }" @click="remove(order)" class="cancel">Ｘ</button>
+      </li>
     </ol>
   </div>
 </template>
@@ -32,13 +35,42 @@ export default {
     },
     print (order) {
       return `${order.name} ${order.temperture} ${order.sugar}`
+    },
+    remove (order) {
+      let index = this.orderList.indexOf(order)
+      this.orderList.splice(index, 1)
+    },
+    orderColor (order) {
+      if (order.temperture.includes('冰')) {
+        return '#ff7575'
+      } else if (order.temperture.includes('熱')) {
+        return '#7575cc'
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+button {
+  outline: none;
+}
+
 .order {
   display: inline-block;
+}
+
+.order-list .order, .order-list .cancel {
+  border-radius: 4px;
+  border: none;
+  color: #FFFFFF;
+  font-size: 20px;
+  padding: 4px 8px;
+  position: relative;
+  text-align: center;
+}
+
+.order-list li {
+  margin: 5px 0;
 }
 </style>
